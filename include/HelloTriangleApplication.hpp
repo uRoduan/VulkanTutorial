ecdb9df28308625186d7c5e1c2943d53ddb26767
uRoduan <928860257@qq.com>
2022-09-23 09:49:40 +0800

    setup

---
+++ include/HelloTriangleApplication.hpp
@@ -0,0 +1,56 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+public:
+
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // not create an OpenGL context
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // disable resize window
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan Tutorial", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+
+    }
+
+    void mainLoop()
+    {
+        while(!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+private:
+    GLFWwindow* window;
+};
