e0b2a10350d9dc0499b247910e0941612a5814e9
uRoduan <928860257@qq.com>
2022-10-29 14:31:28 +0800

    end presentation

--- include/HelloTriangleApplication.hpp
+++ include/HelloTriangleApplication.hpp
@@ -96,6 +96,8 @@ private:
         pickPhysicalDevice();
         createLogicalDevice();
         createSwapChain();
+        createImageView();
+        createGraphicsPipelines();
     }
 
     void createInstance()
@@ -197,6 +199,11 @@ private:
 
     void cleanup()
     {
+        for(auto imageView : swapChainImageViews)
+        {
+            vkDestroyImageView(device, imageView, nullptr);
+        }
+
         vkDestroySwapchainKHR(device, swapChain, nullptr);
         vkDestroyDevice(device, nullptr);
 
@@ -569,6 +576,43 @@ private:
         swapChainExtent = extent;
     }
 
+
+
+    void createImageView()
+    {
+        swapChainImageViews.resize(swapChainImages.size());
+
+        for(size_t i = 0; i < swapChainImages.size(); i++)
+        {
+            VkImageViewCreateInfo createInfo{};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[i];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to create image views");
+            }
+        }
+    }
+
+    void createGraphicsPipelines()
+    {
+
+    }
+
     /*
      * VkDebugUtilsMessageSeverityFlagBitsEXT:
      *      The first parameter specifies the severity of the message, which is one of the following flags:
@@ -617,4 +661,5 @@ private:
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
 };
